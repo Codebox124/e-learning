cb0ab0c07950f87321c4b6300f7c75c936b6b797
sheriff Oladimeji <dimejiademola5@gmail.com>
2023-08-02 15:47:01 +0100

    feat: add colors to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-    
+      colors: {
+        yellow: "#F0A305",
+        darkblue: "#020F24",
+        blue: "#055FF0",
+        brown: "#AC5415",
+        darkslateblue: "#353F50",
+        lightslateblue: "#555E6C",
+      },
     },
   },
   plugins: [],
-}
+};
