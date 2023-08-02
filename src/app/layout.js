1935f5e9fdc7a351667d6b6adbd5e4376e81a4a0
sheriff Oladimeji <dimejiademola5@gmail.com>
2023-08-02 16:18:20 +0100

    feat: add links to navbar

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,9 +1,10 @@
 import Navbar from '@/components/navbar/Navbar'
 import './globals.css'
-import { Inter } from 'next/font/google'
+import { Inter, Poppins_400 } from 'next/font/google'
 import Footer from '@/components/footer/Footer'
 
-const inter = Inter({ subsets: ['latin'] })
+
+const inter = Inter({subsets:["latin"]})
 
 export const metadata = {
   title: 'Create Next App',
