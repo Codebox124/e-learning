f3c469f2b2e5ac7488112b8d745422732b6cea1d
sheriff Oladimeji <dimejiademola5@gmail.com>
2023-08-05 05:43:34 +0100

    feat: add mobile nav

--- src/components/header/HomeHeader.jsx
+++ src/components/header/HomeHeader.jsx
@@ -4,9 +4,10 @@ import Navbar from '../navbar/Navbar'
 import hero from "public/hero.png"
 import Image from 'next/image';
 import Link from 'next/link';
+
 const HomeHeader = () => {
   return (
-    <header className="w-full  flex flex-col md:flex-row justify-between items-center">
+    <header className="container flex-col md:flex-row mt-36 md:mt-8 mb-10">
       <div className="flex-1">
         <h1 className="font-bold text-darkblue text-4xl md:text-5xl">
           Unleash Your <span className="text-yellow">Creativity</span> and Make
@@ -21,7 +22,15 @@ const HomeHeader = () => {
         <Link href="/courses">Explore Courses</Link>
       </div>
 
-      <div className="flex-1"></div>
+      <div className="flex-1 w-full text-center ">
+        
+          <Image
+            src={hero}
+            alt="hero"
+            className="w-full h-[450px] object-cover "
+          />
+      
+      </div>
     </header>
   );
 }
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,8 +1,9 @@
+"use client"
 import Link from 'next/link';
-import React from 'react'
+import React, { useState } from 'react'
 import { BsCart3 } from "react-icons/bs"
 import { GoSearch } from "react-icons/go"
-
+import {AiOutlineClose, AiOutlineMenu} from "react-icons/ai"
 const links = [
   {
     id: 1,
@@ -29,16 +30,25 @@ const links = [
 import  logo from "public/Logo.png"
 import Image from 'next/image';
 const Navbar = () => {
+
+  const [nav, setNav] = useState(false)
+  const handleNav = () => {
+    setNav(!nav)
+  }
   return (
-    <nav className="py-8">
-      <div className="container">
+    <nav className="py-4 fixed top-0 left-0 z-50 w-full bg-white shadow-md md:shadow-none md:relative h-20">
+      <div className="hidden md:flex w-[90%] mx-auto  justify-between items-center">
         <div>
           <Image src={logo} alt="logo" />
         </div>
 
         <div className="flex items-center gap-10 text-lightslateblue ">
           {links.map((link) => (
-            <Link key={link.id} href={link.url} className='font-semibold text-lightslateblue'>
+            <Link
+              key={link.id}
+              href={link.url}
+              className="font-semibold text-lightslateblue"
+            >
               {link.title}
             </Link>
           ))}
@@ -61,6 +71,60 @@ const Navbar = () => {
           <Link
             href="/auth/signup"
             className="p-2 rounded-xl px-6 bg-blue text-white border-none font-semibold "
+          >
+            Sign Up
+          </Link>
+        </div>
+      </div>
+
+      <div className="flex md:hidden w-[90%] mx-auto  justify-between items-center">
+        <div>
+          <Image src={logo} alt="logo" />
+        </div>
+        <div className="flex gap-6 items-center">
+          <Link href="/search">
+            <GoSearch size={30} />
+          </Link>
+          <Link href="/cart">
+            <BsCart3 size={30} />
+          </Link>
+          <button onClick={handleNav}>
+            {nav ? <AiOutlineClose size={30} /> : <AiOutlineMenu size={30} />}
+          </button>
+        </div>
+      </div>
+      <div
+        className={
+          nav
+            ? "md:hidden w-[60%] h-screen bg-white top-0 left-0 fixed z-10 duration-500 px-8 py-8"
+            : "md:hidden w-[50px] h-screen bg-white top-0 left-[-100%] fixed z-10 duration-500"
+        }
+      >
+        <div>
+          <Image src={logo} alt="logo" />
+        </div>
+        <div className="flex flex-col gap-10 z-50 pt-12 ">
+          {links.map((link) => (
+            <Link
+              href={link.url}
+              className="text-[20px]"
+              key={link.id}
+              onClick={handleNav}
+            >
+              {link.title}
+            </Link>
+          ))}
+        </div>
+        <div className="flex gap-8  flex-col mt-6">
+          <Link
+            href="/auth/login"
+            className="border p-2 border-[#B1B5BB] rounded-xl px-6 text-center w-[120px] text-darkblue font-semibold"
+          >
+            Login
+          </Link>
+          <Link
+            href="/auth/signup"
+            className="p-2 rounded-xl px-6 bg-blue text-white border-none  text-center w-[120px] font-semibold "
           >
             Sign Up
           </Link>
